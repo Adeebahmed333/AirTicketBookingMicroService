9d084a393d0d612effaf32100ee6370e311a4fcf
Adeebahmed333 <adeebahmed3337@gmail.com>
2024-02-14 16:13:34 +0530

    Made Basic Structure Of The MicroService

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const apiRoutes=require('./routes/index');
+const db= require('./models/index');
+const {PORT,DB_SYNC}=require('./config/server-config');
+const app=express();
+const setupAndStartServer=()=>{
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.use('./api',apiRoutes);
+    app.listen(PORT,()=>{
+        console.log(`Server Started on PORT: ${PORT}`);
+        if(DB_SYNC==true)
+        {
+            db.sequelize.sync({alter:true});
+        }
+    })
+}
+setupAndStartServer();
